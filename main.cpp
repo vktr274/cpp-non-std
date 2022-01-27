5aabc256efc3cd89e2ded6ac7a1b9fdab4a39dba
Viktor Modroczky <v.modroczky@gmail.com>
2022-01-27 16:37:22 +0100

    Changed examples

--- main.cpp
+++ main.cpp
@@ -4,17 +4,13 @@
 
 int main() {
 	try {
-		nonstd::matrix<double, 3, 4> matrixDouble =
-		{
-			1.1, 2.1, 4.5, 5.9,
-			5.8, 8.9, 3.998, 33.8,
-			0.12, 3.14, 7.24, 76.89
-		};
 		nonstd::matrix<std::string, 2, 2> matrixStr =
 		{
 			"kewk", "bonk",
 			"bruh", "kek"
 		};
+
+		std::cout << matrixStr << '\n';
 
 		nonstd::matrix<uint32_t, 5, 4> matrix =
 		{
@@ -25,7 +21,6 @@ int main() {
 			17, 18, 19, 20
 		};
 		std::cout << matrix << '\n';
-		std::cout << matrix.transpose() << '\n';
 		auto flattened = matrix.flatten();
 
 		for (size_t i = 0; i < flattened.size(); i++) {
@@ -37,14 +32,9 @@ int main() {
 		std::cout << std::endl;
 		std::cout << matrix[2][2] << '\n';
 
-		auto row = matrix[2];
-		for (const auto& value : row) {
-			std::cout << value << ' ';
-		}
-		std::cout << '\n';
-		std::cout << matrix.slice<5, 2>({ }, { 0, 2 });
+		std::cout << matrix.slice<5, 2>({ }, { 0, 2 }) << '\n';
 
-		std::cout << std::endl << matrix.size() << ' ' << matrix.row_size() << ' ' << matrix.column_size() << '\n';
+		std::cout << matrix.size() << ' ' << matrix.row_size() << ' ' << matrix.column_size() << '\n';
 
 		nonstd::matrix<uint16_t, 4, 2> matrixOpTest1 =
 		{
@@ -68,11 +58,12 @@ int main() {
 		std::cout << uint16_t(8) * matrixOpTest1 << '\n';
 		std::cout << matrixOpTest1 * uint16_t(8) << '\n';
 
-		nonstd::matrix<double, 2, 2> resizeTest;
-		resizeTest = { 1, 2, 3, 4 };
-		std::cout << resizeTest << '\n';
-		auto newMatrix = resizeTest.resize<1, 4>(); 
+		auto newMatrix = matrix.resize<2, 10>();
 		std::cout << newMatrix << '\n';
+		auto transposedMatrix = newMatrix.transpose();
+		std::cout << transposedMatrix << '\n';
+		auto originalMatrix = transposedMatrix.transpose().resize<5, 4>();
+		std::cout << originalMatrix << '\n';
 	}
 	catch (const std::length_error& e) {
 		std::cerr << e.what() << '\n';
